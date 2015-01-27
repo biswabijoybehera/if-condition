e75218df101be8decc99c7973d07559a8444b934
infoshinesms@gmail.com <infoshinesms@gmail.com>
2015-01-27 09:15:13 -0800

    if condition

---
+++ if condition/src/demo1.java
@@ -0,0 +1,16 @@
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int test=10;
+     if(test<5)
+
+        {
+	            System.out.println("Biswa");
+        }
+
+        System.out.println("no data found");
+       }
+}
+
